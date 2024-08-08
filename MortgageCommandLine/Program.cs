f2bc0fa6ea8a4fb0b2d04dbf61d2df6b933c7e45
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-08 15:38:13 -0700

    end of week

---
+++ MortgageCommandLine/Program.cs
@@ -0,0 +1,103 @@
+﻿using MortgageCalculator;
+using Spectre.Console;
+using System.CommandLine;
+namespace MortgageCommandLine
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var rootCommand = new RootCommand("Mortgage Calculator Console App");
+            var interactiveCommand = new Command("interactive", "Run calculator interactively.");
+            var batchCommand = new Command("batch", "Run calculator in batch mode.");
+            var loanOption = new Option<decimal?>(
+                  name: "--loan",
+                  description: "the loan amount requested.",
+                  getDefaultValue: () => 200000m);
+            loanOption.AddAlias("-l");
+            var interestOption = new Option<decimal?>(
+                  name: "--interest",
+                  description: "the interest rate express in percentage, ex. 5.5 or 6.2",
+                  getDefaultValue: () => 6m);
+            interestOption.AddAlias("-i");
+            var durationOption = new Option<int?>(
+                  name: "--duration",
+                  description: "loan duration specified in years. default to 15",
+                  getDefaultValue: () => 15
+                  );
+            durationOption.AddAlias("-d");
+
+            batchCommand.Add(loanOption);
+            batchCommand.Add(interestOption);
+            batchCommand.Add(durationOption);
+
+
+            rootCommand.AddCommand(interactiveCommand);
+            rootCommand.AddCommand(batchCommand);
+
+            batchCommand.SetHandler((loanOption, interestOption, durationOption)
+                => { RunBatch(loanOption!.Value, interestOption!.Value, durationOption!.Value); },
+                loanOption,
+                interestOption,
+                durationOption)
+                ;
+            interactiveCommand.SetHandler(() => RunInteractive());
+            int result = rootCommand.InvokeAsync(args).Result;
+
+            return;
+        }
+
+        private static void RunBatch(decimal loanOption, decimal interestOption, int durationOption)
+        {
+            PrintTable(loanOption, interestOption, durationOption);
+        }
+
+        private static void RunInteractive()
+        {
+            decimal loanAmount = AnsiConsole.Ask<decimal>("How much is the [green]loan amount:[/]?", 100000);
+            decimal interest = AnsiConsole.Ask<decimal>("What is the [green]annualized interest rate[/] in percentage?", 5);
+            int durationInYear = AnsiConsole.Ask<int>("How many [green]years[/] is the loan for?", 10);
+            PrintTable(loanAmount, interest, durationInYear);
+        }
+
+        private static void PrintTable(decimal loanAmount, decimal interest, int durationInYear)
+        {
+            Mortgage m = new Mortgage(mortgageOriginationDate: DateTime.Now,
+                                                  originalLoanAmount: loanAmount,
+                                                  durationInMonth: durationInYear * 12,
+                                                  originalInterestRateInPercentage: interest);
+            // Create a table
+            var table = PrepareTable(m);
+            AnsiConsole.Write(table);
+        }
+
+        private static Table PrepareTable(Mortgage m)
+        {
+            var table = new Table();
+
+            // Add some columns
+           
+            table.AddColumn(new TableColumn("[red]Payment Date[/]").Centered());
+            table.AddColumn(new TableColumn("[red]Principle applied[/]").Centered());
+            table.AddColumn(new TableColumn("[red]Interest[/]").Centered());
+            table.AddColumn(new TableColumn("[red]Principle remain[/]").Centered());
+            table.AddColumn(new TableColumn("[red]Interest Paid[/]").Centered());
+            decimal runningInterestTotal = 0;
+            foreach (var p in m.Payments) {
+                runningInterestTotal += p.InterestAmount;
+                table.AddRow(new Markup(p.paymentDate.ToShortDateString()),
+                    new Markup(p.PrincipalAmount.ToString("c")),
+                    new Markup(p.InterestAmount.ToString("c")),
+                    new Markup(p.LoanBalance.ToString("c")),
+                    new Markup(runningInterestTotal.ToString("c")));
+            }
+            // Add some rows
+            
+            
+
+            // Render the table to the console
+            return table;
+        }
+    }
+}
