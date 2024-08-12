fefb8bea10f9b134a65a20a065ff0da64e7ea8eb
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-12 08:55:47 -0700

    make bad code

--- MortgageCalculator/Payment.cs
+++ MortgageCalculator/Payment.cs
@@ -7,14 +7,9 @@ using System.Threading.Tasks;
 namespace MortgageCalculator
 {
    
-        public record struct Payment(int paymentNumber, decimal principalAmount, decimal interestAmount, decimal loanBalance, decimal payment, DateTime paymentDate)
+        public record struct Payment(int paymentNumber, decimal rincipalAmount, decimal interestAmount, decimal loanBalance, decimal payment, DateTime paymentDate)
         {
-            public int PaymentNumber=> paymentNumber;
-            public decimal PrincipalAmount => principalAmount;
-            public decimal InterestAmount => interestAmount;
-            public decimal LoanBalance => loanBalance;
-            public decimal PaymentAmount => payment;
-            public DateTime PaymentDate => paymentDate;
+         
     }
     
 }
